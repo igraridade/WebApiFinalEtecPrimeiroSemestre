b6c4e01b009b19693ffee3b113d409c8cf42a9e3
igraridade <s.blacweel@gmail.com>
2025-05-26 08:56:10 -0300

    Adicionar arquivos de projeto.

---
+++ WebApiFinalEtecPrimeiroSemestre/Models/Base.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApiFinalEtecPrimeiroSemestre.Models
+{
+    public class Base
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ WebApiFinalEtecPrimeiroSemestre/Models/Produtos.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiFinalEtecPrimeiroSemestre.Models
+{
+    public class Produtos : Base
+    {
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
