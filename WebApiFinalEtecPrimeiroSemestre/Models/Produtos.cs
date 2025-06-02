53a9666fe9df04dba688ff3066b2c025ab813d63
igraridade <s.blacweel@gmail.com>
2025-06-02 09:07:29 -0300

    Tipos de dados

--- WebApiFinalEtecPrimeiroSemestre/Models/Produtos.cs
+++ WebApiFinalEtecPrimeiroSemestre/Models/Produtos.cs
@@ -6,5 +6,30 @@
         public string? Descricao { get; set; }
         public int Quantidade { get; set; }
         public decimal Valor { get; set; }
+
+
+        //Tipos de dados
+
+        string nome = "etec"; //Tipo de referencia
+        char letra = 'A';
+
+
+        int valor = 10; //Números inteiros
+        long valor1 = 10L;//Números inteiros 
+        byte valorB = 127;//Números inteiros
+
+        double valorD = 10.0;//Números reais
+        float valorF = 10.0F;//Números reais 
+
+        
+
+
+
+
+
     }
 }
+
+
+
+
