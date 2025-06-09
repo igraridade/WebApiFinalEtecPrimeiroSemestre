9ac11dd05e2a76c4e08ff3d6062b1767eab0a165
igraridade <s.blacweel@gmail.com>
2025-06-09 09:09:19 -0300

    Atualização 09/06

--- WebApiFinalEtecPrimeiroSemestre/Program.cs
+++ WebApiFinalEtecPrimeiroSemestre/Program.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WebApiFinalEtecPrimeiroSemestre.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<WebApiFinalEtecPrimeiroSemestreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiFinalEtecPrimeiroSemestreContext") ?? throw new InvalidOperationException("Connection string 'WebApiFinalEtecPrimeiroSemestreContext' not found.")));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Políticas de Cors Origin
+
+builder.Services.AddCors(etecPoliticas =>
+{
+    etecPoliticas.AddPolicy("EtecAll",
+        builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyMethod();
+        });
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -18,6 +35,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseCors("EtecAll");
 
 app.UseHttpsRedirection();
 
